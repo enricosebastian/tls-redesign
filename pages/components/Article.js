1137029dd0010d6d4858852d644ce682f3cfb08a
enricosebastian <enricosebastiansalazar@gmail.com>
2023-04-02 19:44:09 +0800

    added dynamic pages

--- pages/components/Article.js
+++ pages/components/Article.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Article({article}) {
     console.log(article);
 
     return (
-        <li>{article.title["rendered"]}</li>
+        <li><a href={`/presents/${article.slug}`}>{article.title["rendered"]}</a></li>
     );
 }
