a0e5d73523017be9f264e5203008e948a542b741
enricosebastian <enricosebastiansalazar@gmail.com>
2023-04-02 17:14:05 +0800

    modified to fit cpanel

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = process.env.HOSTNAME
+const hostname = process.env.HOSTNAME || "localhost"
 const port = process.env.PORT || "3000";
 
 // when using middleware `hostname` and `port` must be provided below
